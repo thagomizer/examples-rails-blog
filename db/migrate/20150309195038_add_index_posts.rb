655e3048954ad941522fd7d6ca69e4a2b1619ae7
Aja Hammerly <ajahammerly@google.com>
2015-03-10 10:37:43 -0700

    Uncommenting line commented out to complete deployment

--- db/migrate/20150309195038_add_index_posts.rb
+++ db/migrate/20150309195038_add_index_posts.rb
@@ -1,6 +1,6 @@
 class AddIndexPosts < ActiveRecord::Migration
   def change
-#    add_index :posts, :title
+    add_index :posts, :title
     add_index :posts, :body, :length => {:body => 5 }
     add_index :posts, :created_at
     add_index :posts, :updated_at
