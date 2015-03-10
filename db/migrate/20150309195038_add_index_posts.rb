4271ee90374fd1eaf8568f8705f912a82880e1a6
Aja Hammerly <ajahammerly@google.com>
2015-03-10 10:31:51 -0700

    Add index length to body index

--- db/migrate/20150309195038_add_index_posts.rb
+++ db/migrate/20150309195038_add_index_posts.rb
@@ -1,7 +1,7 @@
 class AddIndexPosts < ActiveRecord::Migration
   def change
     add_index :posts, :title
-    add_index :posts, :body
+    add_index :posts, :body, :length => {:body => 5 }
     add_index :posts, :created_at
     add_index :posts, :updated_at
   end
